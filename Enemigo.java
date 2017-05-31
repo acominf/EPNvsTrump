17500ffe32a0be0fc1a78228eb9897b39f5ce6fc
ldrendonv97 <ldrendonv@gmail.com>
2017-05-31 15:16:59 -0500

    Clase Enemigo

---
+++ Enemigo.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class algo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemigo extends Actor
+{
+    /**
+     * Act - do whatever the algo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    private int cantidadEnemigos;
+    protected int bNivel;
+    
+    public Enemigo()
+    {
+        cantidadEnemigos=10;
+    }
+    
+    public void act() 
+    {
+       
+            
+            
+          
+        
+    }
+        
+       
+}
